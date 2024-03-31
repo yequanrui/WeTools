f640a23ae4dc98dbea520b3dd8e6c7498db40b7c
yequanrui <yequanrui@qq.com>
2024-04-01 02:47:19 +0800

    update

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -2,6 +2,8 @@ import { resolve } from 'path';
 import { defineConfig, bytecodePlugin, externalizeDepsPlugin, splitVendorChunkPlugin } from 'electron-vite';
 import vue from '@vitejs/plugin-vue';
 
+const prefix = `monaco-editor/esm/vs`;
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin(), bytecodePlugin()],
@@ -10,6 +12,16 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin(), bytecodePlugin()],
   },
   renderer: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            editorWorker: [`${prefix}/editor/editor.worker`],
+            jsonWorker: [`${prefix}/language/json/json.worker`],
+          },
+        },
+      },
+    },
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
--- src/renderer/src/views/index.vue
+++ src/renderer/src/views/index.vue
@@ -1,17 +1,10 @@
 <template>
   {{ $t('lang') }}
-  <div>
-    Title: <input id="title" />
-    <TinyButton id="set-title" type="button">Set</TinyButton>
-  </div>
-  <div>
-    <TinyButton id="open-file" type="button">Open a File</TinyButton>
-    File path: <strong id="filePath"></strong>
-  </div>
+  <TinyScrollText text="欢迎使用WeTools"></TinyScrollText>
 </template>
 
 <script setup>
-import TinyButton from '@opentiny/vue-button';
+import TinyScrollText from '@opentiny/vue-scroll-text';
 </script>
 
 <style lang="less" scoped></style>
